20d19ce3c1bf577817f25a554d22e3adabaed66b
nguyenjacobak <nguyenjacobak@gmail.com>
2025-01-09 01:26:43 +0700

    cap nhap 2 forms

--- form_collectdata/form_collect/base/urls.py
+++ form_collectdata/form_collect/base/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('form1',views.form1,name='form1'),
+    path('form2',views.form2,name='form2')
 ]
