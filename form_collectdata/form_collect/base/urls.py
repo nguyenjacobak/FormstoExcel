027df4b85a3433107564f57fd2b3af1bcae1dd19
nguyenjacobak <nguyenjacobak@gmail.com>
2025-01-09 09:20:10 +0700

    them3 forms

--- form_collectdata/form_collect/base/urls.py
+++ form_collectdata/form_collect/base/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns = [
     path('',views.index,name='index'),
     path('form1',views.form1,name='form1'),
-    path('form2',views.form2,name='form2')
+    path('form2',views.form2,name='form2'),
+    path('form3',views.formhd3,name='form3')
 ]
